303cc04ecc1e34f80768f8dcfd3e5bbb5d35bd24
mgregur <marko.gregurovic0304@gmail.com>
2025-01-02 16:46:46 +0100

    move to typescript, setup react-router and material-ui

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from 'pages/home/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
